35b0f8b818ff6d62c7db295926016f24ae872a02
Asrani-Aman <amanasrani1712@gmail.com>
2024-01-19 11:50:39 +0530

    adding data / create blogs/create seed from seeds.rbfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Blog.create(title:"first seed blog", body: "the body of the first seed blog")
+Blog.create(title:"second seed blog", body: "the body of the second seed blog")
